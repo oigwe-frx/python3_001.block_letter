e969042f62d56afaa2639c91a91845a309b5b9e9
Osita Igwe <93604075+oigwe-frx@users.noreply.github.com>
2023-08-03 22:37:57 -0400

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,25 @@
+# Osita Igwe
+# I am learning Danish and Norwegian
+
+first_initial = """
+ OOO
+O   O
+O   O
+O   O
+O   O
+O   O
+ OOO
+""";
+
+last_initial = """ 
+|||||
+  |
+  |
+  |
+  |
+  |
+|||||
+"""
+
+first_initial += last_initial
+print(first_initial);
